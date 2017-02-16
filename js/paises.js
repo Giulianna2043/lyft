45d56051a907eb74dadf21471c3dc881b5a8a5a4
giulianna2043 <giulianna.2043@gmail.com>
2017-02-15 23:09:58 -0500

    avance

---
+++ js/paises.js
@@ -0,0 +1,15 @@
+function init()
+{
+	var lista=document.getElementById("lista-paises");
+	var links=lista.getElementsByTagName("a");
+	
+	for (var i=0; i<links.length; i++)
+	{
+		links.addEventListener("click", obtenerPais)
+	}
+}
+
+function obtenerPais(evt)
+{
+	localStorage.setItem("pais", evt.target)
+}
